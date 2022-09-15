a685f5bc2d4e57f9ecdb3c0f555b9e5121507a24
DerronOnefive <111064439+DerronOnefive@users.noreply.github.com>
2022-09-15 17:35:40 -0400

    Update DIvisibleBy3And5.java

--- DIvisibleBy3And5.java
+++ DIvisibleBy3And5.java
@@ -1,23 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *class: divisibleby3and5
+ *  Author: Derron Pierre
+ * Version: 1.1
+ * Course ITEC 2140
+ * Written : September 15 2022
+ * This program will run days of the week when user inputs a number
  */
 package divisibleby3and5;
 import java.util.Scanner;
 
 
 public class DIvisibleBy3And5 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Scanner in = new Scanner(System.in);
         System.out.print("Enter number:");
         int num = in.nextInt();
-        
+
         if (num% 3 == 0 && num% 5 == 0)
             System.out.println("Divisible by 3 and 5");
         else if (num% 3 == 0)
@@ -26,8 +26,8 @@ public class DIvisibleBy3And5 {
             System.out.println("Divisible by 5 but not by 3");
         else
             System.out.println("Neither divisible by 3 nor by 5");
-        
-        
+
+
     }
-    
+
 }
