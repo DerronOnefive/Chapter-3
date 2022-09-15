f6ba558ae3d67ccef11d7e30991635fcff9bf9e1
DerronOnefive <111064439+DerronOnefive@users.noreply.github.com>
2022-09-15 17:47:47 -0400

    Update RandomNumberGame.java

--- RandomNumberGame.java
+++ RandomNumberGame.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *class: RandomNumberGame
+ *  Author: Derron Pierre
+ * Version: 1.1
+ * Course ITEC 2140
+ * Written : September 15 2022
+ * This program will run to prompt the user to guess randomns number until the guess is right.
  */
 package randomnumbergame;
 import java.util.Random;
@@ -9,40 +12,38 @@ import java.util.Scanner;
 
 public class RandomNumberGame {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
         int answer, guess;
-        
+
         final int Max = 10;
-        
+
         Scanner in = new Scanner(System.in);
-        
+
         Random rand = new Random();
-        
+
         boolean correct = false;
-        
+
         answer = rand.nextInt(Max) + 1;
-        
+
         while (!correct){
-        System.out.println("Guess a Number betwwen 1 and 10:");
-        guess = in.nextInt();
-        if(guess > answer){
-        System.out.println("Too high, you lost");}
-        else if (guess < answer){
+            System.out.println("Guess a Number betwwen 1 and 10:");
+            guess = in.nextInt();
+            if(guess > answer){
+                System.out.println("Too high, you lost");}
+            else if (guess < answer){
                 System.out.println("Too low, you lost");
-                }
-        else{
-            System.out.println("Horray you guessed the number.");
-            correct = true;
-        
-        }
-        
-        
+            }
+            else{
+                System.out.println("Horray you guessed the number.");
+                correct = true;
+
+            }
+
+
         }
         System.exit(0);
     }
-    
+
 }
