40b25f0e7fd2d4a87175d78086cf54b5dcb56a4c
DerronOnefive <111064439+DerronOnefive@users.noreply.github.com>
2022-09-15 17:28:12 -0400

    Update Days.java

--- Days.java
+++ Days.java
@@ -1,24 +1,26 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *class: Alaphabeticalorder
+ *  Author: Derron Pierre
+ * Version: 1.1
+ * Course ITEC 2140
+ * Written : September 15 2022
+ * This program will run days of the week when user inputs a number
  */
 package days;
 import java.util.Scanner;
 
 
+
 public class Days {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
         Scanner in = new Scanner(System.in);
-        
+
         System.out.print("Input number:");
         int day = in.nextInt();
-        
+
         System.out.println(getDayName(day));
     }
     public static String getDayName(int day){
@@ -26,14 +28,14 @@ public class Days {
         switch (day){
             case 1: dayName = "Monday"; break;
             case 2: dayName = "Tuesday"; break;
-                case 3: dayName = "Wednesday"; break;
+            case 3: dayName = "Wednesday"; break;
             case 4: dayName = "Thursday"; break;
-                case 5: dayName = "Friday"; break;
+            case 5: dayName = "Friday"; break;
             case 6: dayName = "Saturday"; break;
-                case 7: dayName = "Sunday"; break;
-                default:dayName = "Invalid day range";
+            case 7: dayName = "Sunday"; break;
+            default:dayName = "Invalid day range";
         }
         return dayName;
     }
-    
+
 }
