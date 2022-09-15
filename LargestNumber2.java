628496e9c003adce644a30371b5a9259a48c2518
DerronOnefive <111064439+DerronOnefive@users.noreply.github.com>
2022-09-15 17:41:09 -0400

    Update LargestNumber2.java

--- LargestNumber2.java
+++ LargestNumber2.java
@@ -1,7 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *class: LargestNumber2
+ *  Author: Derron Pierre
+ * Version: 1.1
+ * Course ITEC 2140
+ * Written : September 15 2022
+ * This program will run the largest number input
  */
 package largestnumber2;
 
@@ -10,29 +13,27 @@ import java.util.Scanner;
 
 public class LargestNumber2 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        
-         int a,b,c,largest,temp;
+
+
+        int a,b,c,largest,temp;
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Enter firts number:");
+
+        System.out.println("Enter first number:");
         a=sc.nextInt();
-        
+
         System.out.println("Enter second number:");
         b=sc.nextInt();
-        
+
         System.out.println("Enter third number:");
         c=sc.nextInt();
-        
+
         temp=a>b?a:b;
-        
+
         largest=c>temp?c:temp;
-        
+
         System.out.println("The largest number is:" +largest);
     }
-    
+
 }
